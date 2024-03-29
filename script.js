47110ace589f0acf2c075c93e596d03ae12f4b44
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-03-29 12:32:28 -0500

    Change tileTown keywords to this keyword script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let tileTown = {
         darkSquareColor: "hsl(240, 50%, 50%)"
     },
     Piece: function (href, square, newId, newClass) {
-        idTree.chessBoard.innerHTML += `<image class="${newClass}" href="${href}" id="${newId}" width="${tileTown.board.pieceWidth}" height="${tileTown.board.pieceWidth}"/>`;
-        tileTown.movePiece(id(newId), square);
+        idTree.chessBoard.innerHTML += `<image class="${newClass}" href="${href}" id="${newId}" width="${this.board.pieceWidth}" height="${this.board.pieceWidth}"/>`;
+        this.movePiece(id(newId), square);
         this.coordinate = square;
     },
     movePiece: function (object, square) {
-        object.setAttribute("x", +idTree[square].getAttribute("x") + tileTown.board.piecePadding);
-        object.setAttribute("y", +idTree[square].getAttribute("y") + tileTown.board.piecePadding);
+        object.setAttribute("x", +idTree[square].getAttribute("x") + this.board.piecePadding);
+        object.setAttribute("y", +idTree[square].getAttribute("y") + this.board.piecePadding);
     },
     numberToFile: function (number) {
         let file;
@@ -67,27 +67,27 @@ let tileTown = {
     },
     addPieces: function () {
         for (let i = 0; i < 8; i++) {
-            let newFile = tileTown.numberToFile(i);
-            tileTown.pieces.white.pawns[newFile] = new this.Piece("assets/pawn-w.svg", `${newFile}2`, `whitePawn${newFile.toUpperCase()}`, "white pawn");
-            tileTown.pieces.black.pawns[newFile] = new this.Piece("assets/pawn-b.svg", `${newFile}7`, `blackPawn${newFile.toUpperCase()}`, "black pawn");
+            let newFile = this.numberToFile(i);
+            this.pieces.white.pawns[newFile] = new this.Piece("assets/pawn-w.svg", `${newFile}2`, `whitePawn${newFile.toUpperCase()}`, "white pawn");
+            this.pieces.black.pawns[newFile] = new this.Piece("assets/pawn-b.svg", `${newFile}7`, `blackPawn${newFile.toUpperCase()}`, "black pawn");
         }
         
-        tileTown.pieces.white.rookQ = new this.Piece("assets/rook-w.svg", `a1`, `whiteRookQ`, "white rook");
-        tileTown.pieces.white.rookK = new this.Piece("assets/rook-w.svg", `h1`, `whiteRookK`, "white rook");
-        tileTown.pieces.black.rookQ = new this.Piece("assets/rook-b.svg", `a8`, `blackRookQ`, "black rook");
-        tileTown.pieces.black.rookK = new this.Piece("assets/rook-b.svg", `h8`, `blackRookK`, "black rook");
-        tileTown.pieces.white.knightQ = new this.Piece("assets/knight-w.svg", `b1`, `whiteKnightQ`, "white knight");
-        tileTown.pieces.white.knightK = new this.Piece("assets/knight-w.svg", `g1`, `whiteKnightK`, "white knight");
-        tileTown.pieces.black.knightQ = new this.Piece("assets/knight-b.svg", `b8`, `blackKnightQ`, "black knight");
-        tileTown.pieces.black.knightK = new this.Piece("assets/knight-b.svg", `g8`, `blackKnightK`, "black knight");
-        tileTown.pieces.white.bishopQ = new this.Piece("assets/bishop-w.svg", `c1`, `whiteBishopQ`, "white bishop");
-        tileTown.pieces.white.bishopK = new this.Piece("assets/bishop-w.svg", `f1`, `whiteBishopK`, "white bishop");
-        tileTown.pieces.black.bishopQ = new this.Piece("assets/bishop-b.svg", `c8`, `blackBishopQ`, "black bishop");
-        tileTown.pieces.black.bishopK = new this.Piece("assets/bishop-b.svg", `f8`, `blackBishopK`, "black bishop");
-        tileTown.pieces.white.queen = new this.Piece("assets/queen-w.svg", `d1`, `whiteQueen`, "white queen");
-        tileTown.pieces.white.king = new this.Piece("assets/king-w.svg", `e1`, `whiteKing`, "white king");
-        tileTown.pieces.black.queen = new this.Piece("assets/queen-b.svg", `d8`, `blackQueen`, "black queen");
-        tileTown.pieces.black.king = new this.Piece("assets/king-b.svg", `e8`, `blackKing`, "black king");
+        this.pieces.white.rookQ = new this.Piece("assets/rook-w.svg", `a1`, `whiteRookQ`, "white rook");
+        this.pieces.white.rookK = new this.Piece("assets/rook-w.svg", `h1`, `whiteRookK`, "white rook");
+        this.pieces.black.rookQ = new this.Piece("assets/rook-b.svg", `a8`, `blackRookQ`, "black rook");
+        this.pieces.black.rookK = new this.Piece("assets/rook-b.svg", `h8`, `blackRookK`, "black rook");
+        this.pieces.white.knightQ = new this.Piece("assets/knight-w.svg", `b1`, `whiteKnightQ`, "white knight");
+        this.pieces.white.knightK = new this.Piece("assets/knight-w.svg", `g1`, `whiteKnightK`, "white knight");
+        this.pieces.black.knightQ = new this.Piece("assets/knight-b.svg", `b8`, `blackKnightQ`, "black knight");
+        this.pieces.black.knightK = new this.Piece("assets/knight-b.svg", `g8`, `blackKnightK`, "black knight");
+        this.pieces.white.bishopQ = new this.Piece("assets/bishop-w.svg", `c1`, `whiteBishopQ`, "white bishop");
+        this.pieces.white.bishopK = new this.Piece("assets/bishop-w.svg", `f1`, `whiteBishopK`, "white bishop");
+        this.pieces.black.bishopQ = new this.Piece("assets/bishop-b.svg", `c8`, `blackBishopQ`, "black bishop");
+        this.pieces.black.bishopK = new this.Piece("assets/bishop-b.svg", `f8`, `blackBishopK`, "black bishop");
+        this.pieces.white.queen = new this.Piece("assets/queen-w.svg", `d1`, `whiteQueen`, "white queen");
+        this.pieces.white.king = new this.Piece("assets/king-w.svg", `e1`, `whiteKing`, "white king");
+        this.pieces.black.queen = new this.Piece("assets/queen-b.svg", `d8`, `blackQueen`, "black queen");
+        this.pieces.black.king = new this.Piece("assets/king-b.svg", `e8`, `blackKing`, "black king");
     
         setupTree();
     },
