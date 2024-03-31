fcdd08c8fa71908b00467baff3099c6ffc765697
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-03-30 21:50:22 -0500

    Fixed code that made class names for black pawns incorrect script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ let tileTown = {
         for (let i = 0; i < 8; i++) {
             let newFile = this.numberToFile(i);
             tileTown.pieces.white.pawns[newFile] = new tileTown.Piece("assets/pawn-w.svg", `${newFile}2`, `whitePawn${newFile.toUpperCase()}`, "white pawn piece");
-            tileTown.pieces.black.pawns[newFile] = new tileTown.Piece("assets/pawn-b.svg", `${newFile}7`, `blackPawn${newFile.toUpperCase()}`, "black pawn");
+            tileTown.pieces.black.pawns[newFile] = new tileTown.Piece("assets/pawn-b.svg", `${newFile}7`, `blackPawn${newFile.toUpperCase()}`, "black pawn piece");
         }
         
         tileTown.pieces.white.rookQ = new tileTown.Piece("assets/rook-w.svg", `a1`, `whiteRookQ`, "white rook piece");
