319cb86f480fad0a00acf116c678125af28d51da
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-05-20 12:56:13 -0500

    Fixed bug with SAN notation removing captures script.js

--- script.js
+++ script.js
@@ -319,11 +319,16 @@ let tileTown = {
         let allPieces = document.getElementsByClassName("piece");
 
         for (let i = 0; i < allPieces.length; i++) {
+            if (allPieces[i].id.includes(san.slice(2))) {
+                tileTown.removePiece(id(allPieces[i].id), allPieces[i].id);
+            }
             if (allPieces[i].id.includes(san.slice(0, 2))) {
                 tileTown.movePiece(id(allPieces[i].id), san.slice(2));
                 break;
             }
         }
+
+        
     }
 }
 
