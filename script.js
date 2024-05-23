b956df8a6dec0f8d0f27223dbfdfca4924debb7f
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-05-23 11:31:47 -0500

    Fixed width declaration for div

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ let tileTown = {
         let tiles = document.getElementsByClassName("tile");
         let pieces = document.getElementsByClassName("piece");
 
-        tileTown.board.size = document.getElementById("wrapper").style.width.slice(0, -2);
+        tileTown.board.size = document.getElementById("wrapper").getAttribute("width");
         document.getElementById("chessBoard").setAttribute("width", tileTown.board.size);
         document.getElementById("chessBoard").setAttribute("height", tileTown.board.size);
         tileTown.board.squareWidth = +tileTown.board.size / 8;
