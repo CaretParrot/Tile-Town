f829097db69412589bfef806c60d9d0682b2ba5c
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-05-28 17:25:26 -0500

    Added debugger for data-coordinate in clickPieceHandler script.js

--- script.js
+++ script.js
@@ -216,6 +216,7 @@ function clickPieceHandler(object) {
         tileTown.board.selectedPieceId = "";
     } else if (tileTown.board.selectedPieceId !== object.id) {
         tileTown.movePiece(id(tileTown.board.selectedPieceId), object["data-coordinate"]);
+        console.log(object["data-coordinate"]);
         tileTown.removePiece(object);
         tileTown.board.selectedPieceId = "";
     }
